d5c9c052337e40e35cc5be738b1d1eb3026743fe
Zachary Levonian <zach@digitalharbor.org>
2025-04-01 17:35:40 -0700

    max depth exploration

--- src/misc/src/levon_misc/leetcode/april2025.py
+++ src/misc/src/levon_misc/leetcode/april2025.py
@@ -1,4 +1,17 @@
-from typing import List
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(
+        self,
+        val=0,
+        left: Optional["TreeNode"] = None,
+        right: Optional["TreeNode"] = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def rotate1(nums: List[int], k: int) -> List[int]:
@@ -45,3 +58,62 @@ def rotate2(nums: List[int], k: int) -> None:
         curr_value = next_value
         curr_index = target_index
         n_replaced += 1
+
+
+class MaxDepthOfBinaryTree:
+    def maxDepth1(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        return self.maxDepthRecursive(root, 0)
+
+    def maxDepthRecursive(self, node: Optional[TreeNode], depth: int) -> int:
+        if node is None:
+            return depth
+        left_depth = self.maxDepthRecursive(node.left, depth + 1)
+        right_depth = self.maxDepthRecursive(node.right, depth + 1)
+        depth = max((left_depth, right_depth))
+        return depth
+
+    def maxDepth2(self, root: Optional[TreeNode]) -> int:
+        """maxDepth1 works fine, but the need to track the depth in a separate variable is unfortunate.
+        Do we really need to track it in a function parameter?
+        No, because intuitively we don't need to know our current depth, which implies we can put the "+1" in the return statement.
+        """
+        if root is None:
+            return 0
+        return max(self.maxDepth2(root.left), self.maxDepth2(root.right)) + 1
+
+    def maxDepth3(self, root: Optional[TreeNode]) -> int:
+        """maxDepth2 uses recursion, which will start running us into max stack size issues.
+        What about an iterative implementation of Breadth First Search?"""
+        if root is None:
+            return 0
+        queue: deque[tuple[TreeNode | None, int]] = deque([(root, 1)])
+        max_depth = 0
+        while len(queue) > 0:
+            node, depth = queue.popleft()
+            if depth > max_depth:
+                max_depth = depth
+            if node.left is not None:
+                queue.append((node.left, depth + 1))
+            if node.right is not None:
+                queue.append((node.right, depth + 1))
+        return max_depth
+
+    def maxDepth4(self, root: Optional[TreeNode]) -> int:
+        """maxDepth2 uses recursion, which will start running us into max stack size issues.
+        What about an iterative implementation of Depth First Search?
+        Note we can just use a list, as pop()"""
+        if root is None:
+            return 0
+        stack: list[tuple[TreeNode | None, int]] = [(root, 1)]
+        max_depth = 0
+        while len(stack) > 0:
+            node, depth = stack.pop()
+            if depth > max_depth:
+                max_depth = depth
+            if node.left is not None:
+                stack.append((node.left, depth + 1))
+            if node.right is not None:
+                stack.append((node.right, depth + 1))
+        return max_depth
