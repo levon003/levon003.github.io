a140bc6a3c12f1d5bf86b6a3625de9df98ed0cbc
Zachary Levonian <zach@digitalharbor.org>
2025-04-02 17:54:33 -0700

    Add implementation of count tracker

---
+++ src/misc/src/levon_misc/streaming/tracker.py
@@ -0,0 +1,77 @@
+class PositiveCountTracker:
+    """
+    Keeps track of the highest value item given a stream
+    of increases and decreases associated with particular content IDs
+    (e.g. likes and dislikes on videos).
+
+    To implement this spec, we need a data structure that can:
+    -Insert and pop rapidly - O(1)
+    -Get most popular item - O(1)
+
+    We accomplish this with two Hash Maps:
+    - The first maps content IDs to the current count of that content ID.
+    - The second maps counts to the set of content IDs currently at that count.
+    Finally, we keep track of the highest count of any tracked content ID.
+
+    In Java, we might consider use a TreeMap, which implements the SortedMap interface.
+    However, that leaves insertions, deletions, and search at O(log n).
+    """
+
+    def __init__(self):
+        self.counts: dict[int, int] = {}  # content_id -> count
+        self.content_ids: dict[int, set[int]] = (
+            {}
+        )  # count -> {content_id1, content_id2, etc.}
+        self.max_count = 0
+
+    def get_largest(self) -> int:
+        """Get the largest bit.
+
+        Returns:
+            int: Returns the content ID with the highest count, or -1 if no content with a non-zero count.
+        """
+        if len(self.counts) == 0:
+            # no content ID with a positive value
+            return -1
+        assert self.max_count > 0
+        return next(iter(self.content_ids[self.max_count]))
+
+    def _set_new_count(self, content_id: int, prev_count: int, new_count: int):
+        if new_count > 0:
+            if new_count not in self.content_ids:
+                self.content_ids[new_count] = set()
+            self.content_ids[new_count].add(content_id)
+            self.counts[content_id] = new_count
+        else:
+            # count at zero, so stop counting this content_id
+            del self.counts[content_id]
+        if prev_count > 0:
+            self.content_ids[prev_count].remove(content_id)
+            if len(self.content_ids[prev_count]) == 0:
+                del self.content_ids[prev_count]
+
+    def increase(self, content_id: int) -> None:
+        if content_id < 0:
+            raise ValueError(f"Content ID {content_id} not positive.")
+        if content_id not in self.counts:
+            self.counts[content_id] = 0
+        prev_count = self.counts[content_id]
+        new_count = prev_count + 1
+        self._set_new_count(content_id, prev_count, new_count)
+        if new_count > self.max_count:
+            self.max_count = new_count
+
+    def decrease(self, content_id: int):
+        if content_id < 0:
+            raise ValueError(f"Content ID {content_id} not positive.")
+        if content_id not in self.counts:
+            # we never track negative counts
+            return
+        prev_count = self.counts[content_id]
+        new_count = prev_count - 1
+        self._set_new_count(content_id, prev_count, new_count)
+        if prev_count == self.max_count:
+            # this content ID was the previous max
+            if self.max_count not in self.content_ids:
+                # there's no other content ID at this value
+                self.max_count = new_count
